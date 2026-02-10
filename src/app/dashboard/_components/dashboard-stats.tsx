9096346f8a5a99c1a92fc0c1c6104259f0b6fe7b
Bruno Erceg <brunoerceg95@gmail.com>
2026-02-10 19:52:55 +0100

    refactor(DashboardStats): add awards and change display

--- src/app/dashboard/_components/dashboard-stats.tsx
+++ src/app/dashboard/_components/dashboard-stats.tsx
@@ -1,44 +1,43 @@
 "use client";
-import { EyeIcon, ThumbsUp } from "lucide-react";
+import { EyeIcon, ThumbsUp, Trophy } from "lucide-react";
 
 import { WebsitesStats } from "@/types/websites";
 
 import { StatsCard } from "@/components/ui/stats-card";
 
 export function DashboardStats({ stats }: { stats: WebsitesStats }) {
-  const monthLikesChange = stats.thisMonthLikes - stats.lastMonthLikes;
+  const STATS_CONFIG = [
+    {
+      title: "Sveukupni pregledi",
+      value: stats.totalViews,
+      Icon: EyeIcon,
+    },
+    {
+      title: "Sveukupni lajkovi",
+      value: stats.totalLikes,
+      Icon: ThumbsUp,
+    },
+    {
+      title: "Mjesečni lajkovi",
+      value: stats.thisMonthLikes,
+      Icon: ThumbsUp,
+    },
+    {
+      title: "Nagrade",
+      value: stats.totalLikes, // Jesi siguran da ovdje ne ide neki drugi field?
+      Icon: Trophy,
+    },
+  ];
   return (
     <div className="flex justify-between gap-5">
-      <StatsCard
-        title="Sveukupni pregledi"
-        value={stats.totalViews}
-        icon={<EyeIcon className="size-4" />}
-      />
-      <StatsCard
-        title="Sveukupni lajkovi"
-        value={stats.totalLikes}
-        icon={<ThumbsUp className="size-4" />}
-      />
-      <StatsCard
-        title="Mjesečni lajkovi"
-        value={stats.thisMonthLikes}
-        icon={<ThumbsUp className="size-4" />}
-        subtitle={
-          monthLikesChange > 0 ? (
-            <span className="text-green-600">
-              +{monthLikesChange} više nego zadnji mjesec
-            </span>
-          ) : monthLikesChange < 0 ? (
-            <span className="text-red-600">
-              {monthLikesChange} manje nego prošli mjesec
-            </span>
-          ) : (
-            <span className="text-muted-foreground">
-              Isto kao i prošli mjesec
-            </span>
-          )
-        }
-      />
+      {STATS_CONFIG.map((stat, index) => (
+        <StatsCard
+          key={index}
+          title={stat.title}
+          value={stat.value}
+          Icon={stat.Icon}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ui/stats-card.tsx
+++ src/components/ui/stats-card.tsx
@@ -1,40 +1,37 @@
 import {
   DollarSign,
-  Users,
-  ShoppingCart,
-  TrendingUp,
-  ThumbsUp,
   EyeIcon,
+  LucideIcon,
+  ShoppingCart,
+  ThumbsUp,
+  TrendingUp,
+  Users,
 } from "lucide-react";
+
 import { cn } from "@/lib/utils";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface StatsCardProps {
   title: string;
   value: string | number | undefined;
-  subtitle?: React.ReactNode;
-  icon: React.ReactNode;
+  Icon: LucideIcon;
   className?: string;
 }
 
-export function StatsCard({
-  title,
-  value,
-  subtitle,
-  icon,
-  className,
-}: StatsCardProps) {
+export function StatsCard({ title, value, Icon, className }: StatsCardProps) {
   return (
-    <Card className={cn("w-full max-w-xs", className)}>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium text-muted-foreground">
+    <Card className={cn("w-full", className)}>
+      <CardHeader className="flex flex-row items-center justify-between">
+        <CardTitle className="text-muted-foreground text-sm font-medium">
           {title}
         </CardTitle>
-        <div className="rounded-md bg-primary/10 p-2">{icon}</div>
+        <div className="bg-primary/10 rounded-md p-2">
+          <Icon className="size-4" />
+        </div>
       </CardHeader>
       <CardContent>
         <div className="text-3xl font-bold">{value}</div>
-        <p className="mt-1 text-sm text-muted-foreground">{subtitle}</p>
       </CardContent>
     </Card>
   );
